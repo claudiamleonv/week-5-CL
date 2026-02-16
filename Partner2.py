79aa9c60d188af4342724f1bb538c50203cb19c2
claudiamleonv <claudiamleonv@gmail.com>
2026-02-16 18:07:12 +0000

    This is my change
    Co-authored-by: Oliverflo61 <Oliverflo61@users.noreply.github.com>

---
+++ Partner2.py
@@ -0,0 +1,26 @@
+#this is a comment in Python! 
+
+"""
+Partner 1: Oliver Flores
+Partner 2: Claudia Leon
+"""
+# Some more single- line comment
+
+print("Hello, World. I like pancakes")
+
+
+
+
+
+
+"""
+*** Multi-line comments are cool too
+
+
+
+see?????
+
+
+Whattttt 
+"""
+
