5d5f66a05f8e45d3aaa11bc6a213d0c4621301e1
claudiamleonv <claudiamleonv@gmail.com>
2026-02-18 18:04:34 +0000

    In class assignment 2/18

---
+++ inclass.py
@@ -0,0 +1,29 @@
+# Example 1:
+name="Claudia"
+
+# Example 2:
+num=36
+
+# Example 3:
+print("Hello, World")
+
+# Example 4:
+print(name)
+
+# Example 5:
+print("Hello there, my name is", name)
+
+# Example 6:
+name="Cloud"
+print("My nickname is", name)
+
+# Example 7:
+print("what is your favorite animal?")
+animal=input()
+print("I didn't know that", animal, "what your favorite animal!")
+
+# Example 8:
+if num>10:
+    print("Congrats, your number is greater than 10!")
+else:
+    print("Sorry, your number is not greater than 10")
---
+++ partner2_02-18.py
@@ -0,0 +1,37 @@
+# 1. Print a message to the screen
+print("I like to play Tennis")
+# 2. Change the message to your own words and run again
+print ("Your own words and run again")
+# 3. Print your favorite food
+print("My favorite food is pizza")
+# 4. Try printing a number instead of text
+num1=21
+print(num1)
+# 5. Create a variable that stores your name
+name="Claudia"
+# 6. Print the variable
+print(name)
+# 7. Change the name inside the variable and run again
+name="Cloud"
+# 8. Create a variable with a number
+num=61
+# 9. Print the age
+print(num)
+# 10. Add 2 to the age and print the result
+print(num+2)
+# 11. Ask the user for their name
+print("What is your name?")
+# 12. Print a greeting using their name
+print("My name is", name, "I am", num1, "years old!")
+# 13. Ask the user for a number
+print("What is your favorite number?")
+num3=input()
+# 14. Print the number they typed
+print(num3)
+# 15. Print a goodbye message
+print("Adios amigo!")
+
+
+
+# // https://www.w3schools.com/python/python_intro.asp website used for this
+# https://www.geeksforgeeks.org/python/input-and-output-in-python/
